8448830a6d87d411b05a000fae4c5da513c8fb78
holysugar <holysugar@gmail.com>
2012-12-20 15:01:21 +0900

    Implement reading comment

--- lib/active_record-mysql_comment.rb
+++ lib/active_record-mysql_comment.rb
@@ -1,7 +1,31 @@
+require "active_record"
+require "active_record/connection_adapters/abstract_mysql_adapter"
 require "active_record-mysql_comment/version"
 
 module ActiveRecord
-  module MysqlComment
-    # Your code goes here...
+  module ConnectionAdapters
+    class AbstractMysqlAdapter
+      class Column
+
+        # not using attr_reader to avoid change initialize interface,
+        # which is different between version 3.x and 4.x.
+        attr_accessor :comment
+      end
+
+      # overwrite
+      def columns(table_name, name = nil)#:nodoc:
+        sql = "SHOW FULL FIELDS FROM #{quote_table_name(table_name)}"
+        execute_and_free(sql, 'SCHEMA') do |result|
+          each_hash(result).map do |field|
+            c = new_column(field[:Field], field[:Default], field[:Type], field[:Null] == "YES", field[:Collation])
+            c.comment = field[:Comment]
+            c
+          end
+        end
+
+      end
+
+    end
+
   end
 end
--- spec/reading_comment_spec.rb
+++ spec/reading_comment_spec.rb
@@ -1,14 +1,21 @@
+# coding: utf-8
+
 require 'spec_helper'
 require 'models/test_model'
 
 describe TestModel do
-  describe 'column object' do
+  describe 'column object of comment_string' do
     subject { TestModel.columns.find{|c| c.name == 'comment_string' } }
-
-    it "" do
-      subject.comment.should == 'column comment'
-    end
-
+    it { subject.comment.should == 'column comment' }
   end
+
+  describe 'column object of ja_comment_string' do
+    subject { TestModel.columns.find{|c| c.name == 'ja_comment_string' } }
+    it { subject.comment.should == 'コメント' }
+    it "encoding is utf-8" do
+      subject.comment.encoding.should == Encoding::UTF_8
+    end
+  end
+
 end
 
